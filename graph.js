8b90e6053d2198862592845b645a9077f03d55ae
Julie Han <juliemenghan@gmail.com>
2022-09-22 15:33:45 -0400

    all test pass up to last problem

--- graph.js
+++ graph.js
@@ -7,7 +7,6 @@ class Node {
   }
 }
 
-
 /** Graph class. */
 
 class Graph {
@@ -16,32 +15,100 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) { }
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) { }
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) { }
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   /** remove vertex from graph:
    *
    * - remove it from nodes property of graph
    * - update any adjacency lists using that vertex
    */
-  removeVertex(vertex) { }
+  removeVertex(vertex) {
+    if (vertex.adjacent.size > 0) {
+      vertex.adjacent.forEach((v) => this.removeEdge(v, vertex));
+    }
+    this.nodes.delete(vertex);
+  }
 
   /** traverse graph with DFS and returns array of Node values */
-  depthFirstSearch(start) { }
+  depthFirstSearch(start, seen = new Set([start]), visited = [start.value]) {
+    for (let neighbor of start.adjacent) {
+      if (!seen.has(neighbor)) {
+        seen.add(neighbor);
+        visited.push(neighbor.value);
+        return this.depthFirstSearch(neighbor, seen, visited);
+      }
+    }
+    return visited;
+  }
 
   /** traverse graph with BDS and returns array of Node values */
-  breadthFirstSearch(start) { }
+  breadthFirstSearch(start) {
+    let toVisitQueue = [start];
+    let seen = new Set([start]);
+    let visited = [start.value];
+
+    while (toVisitQueue.length > 0) {
+      let current = toVisitQueue.shift();
+
+      for (let neighbor of current.adjacent) {
+        if (!seen.has(neighbor)) {
+          seen.add(neighbor);
+          visited.push(neighbor.value);
+        }
+      }
+    }
+    return visited;
+  }
 
   /** find the distance of the shortest path from the start vertex to the end vertex */
-  distanceOfShortestPath(start, end) { }
+  distanceOfShortestPath(
+    start,
+    end,
+    seen = new Set([start]),
+    minLength = null,
+    length = 0
+  ) {
+    if (start === end) {
+      minLength = minLength && minLength < length ? minLength : length;
+    }
+
+    for (let neighbor of start.adjacent) {
+      console.log("seen", seen);
+      if (!seen.has(neighbor)) {
+        seen.add(neighbor);
+        length = length + 1;
+        return this.distanceOfShortestPath(
+          neighbor,
+          end,
+          seen,
+          minLength,
+          length
+        );
+      }
+    }
+    return minLength;
+  }
 }
 
-module.exports = { Graph, Node }
+module.exports = { Graph, Node };
